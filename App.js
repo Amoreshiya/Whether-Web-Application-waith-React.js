491e1d7141d74952221045dc31e0c49653109bf7
Amoreshiya <105770197+Amoreshiya@users.noreply.github.com>
2022-10-04 18:31:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+
+import React, { StrictMode } from 'react';
+import './App.css';
+import Tempapp from './Componanats/Tempapp';
+
+
+function App() {
+  return (
+    <div className="App">
+      <StrictMode>
+      
+      <Tempapp/>
+
+      </StrictMode> 
+      
+    </div>
+  );
+}
+
+export default App;
